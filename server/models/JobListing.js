4558a6959dd1b41dbe08a8dc5000bf81cac304b5
chetana <kumbharchetana2001@gmail.com>
2024-08-28 21:06:59 +0530

    updated

---
+++ server/models/JobListing.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const JobListingSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  company: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  requirements: {
+    type: String,
+    required: true,
+  },
+  salaryRange: {
+    type: String,
+    required: true,
+  },
+  postedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('JobListing', JobListingSchema);
